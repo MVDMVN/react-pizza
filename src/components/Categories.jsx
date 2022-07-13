d44f541f7904326bc0a3f917396ba489020f8ef1
MVDMVN <mvdmvn@MacBook-Pro-MVDMVN.local>
2022-07-13 22:15:35 +0300

    main components, click logic for types and sizes

---
+++ src/components/Categories.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = useState();
+
+  const categories = ['Все', 'Мясные', 'Вегатерианские', 'Гриль', 'Острые', 'Закрытые'];
+
+  const onClickHandle = (index) => {
+    setActiveIndex(index);
+  }
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, i) => (
+          <li onClick={() => onClickHandle(i)} className={activeIndex === i ? "active" : ''}>
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
